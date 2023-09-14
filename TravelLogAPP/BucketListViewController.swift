310efc33616b468ccedb6e15d14b8a7f2fcb16b0
Seoon KIM <6872065@gmail.com>
2023-09-14 16:10:57 +0900

    feat: BucketListVC add Task

--- TravelLogAPP/BucketListViewController.swift
+++ TravelLogAPP/BucketListViewController.swift
@@ -27,6 +27,12 @@ class BucketListViewController: UIViewController {
         return tableView
     }()
     
+    private let addButton: UIBarButtonItem = {
+        let addButton = UIBarButtonItem()
+        addButton.image = UIImage(systemName: "plus.circle.fill")
+        return addButton
+    }()
+    
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -35,11 +41,43 @@ class BucketListViewController: UIViewController {
         setupUI()
         tableView.dataSource = self
         tableView.delegate = self
+        fetchTask()
+        
+        addButton.target = self
+        addButton.action = #selector(didTapAddButton)
+    }
+    
+    
+    // MARK: - Tapped Action
+    @objc private func didTapAddButton() {
+        let alert = UIAlertController(title: "Add Bucket List", message: "", preferredStyle: .alert)
+        alert.addTextField { textField in
+            textField.placeholder = "Please, write your bucket list."
+        }
+        let add = UIAlertAction(title: "Add", style: .default) { _ in
+            let textField = alert.textFields![0]
+            let newBucketList = Task(context: self.context)
+            newBucketList.title = textField.text
+            newBucketList.createDate = Date()
+            newBucketList.isCompleted = false
+            
+            do {
+                try self.context.save()
+            } catch {
+                print("🚨 Error: Save task")
+            }
+            self.fetchTask()
+            
+        }
+        alert.addAction(add)
+        self.present(alert, animated: true, completion: nil)
     }
     
     
     // MARK: - setupUI
     private func setupUI() {
+        self.navigationItem.title = "✍🏻 Bucket List ✍🏻"
+        self.navigationItem.rightBarButtonItem = addButton
         self.view.addSubview(tableView)
         
         tableView.snp.makeConstraints { make in
@@ -49,17 +87,20 @@ class BucketListViewController: UIViewController {
     }
     
     
-    // MARK: - Fetch Data
+    // MARK: - Core Data
     private func fetchTask() {
         let request = Task.fetchRequest()
         
         do {
             self.bucketList = try context.fetch(request)
-            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         } catch {
-            print("Fetch Task Error")
+            print("🚨 Error: Fetch Task")
         }
     }
+    
 
 }
 
@@ -72,6 +113,7 @@ extension BucketListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "bucketListCell", for: indexPath)
         let task = bucketList![indexPath.row]
+        cell.backgroundColor = UIColor(red: 248/255, green: 248/255, blue: 248/255, alpha: 255)
         cell.textLabel?.text = task.title
         if task.isCompleted == true {
             cell.textLabel?.textColor = .systemGray2
