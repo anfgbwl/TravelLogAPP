ffae5be39d873825790bf3459f7994050a9a07f0
Seoon KIM <6872065@gmail.com>
2023-09-14 10:50:58 +0900

    feat: 초기 ViewController 생성

---
+++ TravelLogAPP/ProfileDesignViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileDesignViewController.swift
+//  TravelLogAPP
+//
+//  Created by t2023-m0076 on 2023/09/14.
+//  Profile & Travel Log Page
+
+import UIKit
+
+class ProfileDesignViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TravelLogAPP/ViewController.swift
+++ TravelLogAPP/ViewController.swift
@@ -3,7 +3,7 @@
 //  TravelLogAPP
 //
 //  Created by t2023-m0076 on 2023/09/14.
-//
+//  Main Page
 
 import UIKit
 
